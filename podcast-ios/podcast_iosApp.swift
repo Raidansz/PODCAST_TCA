82961a1e7244453da6b5fa3965e17508c5e2a5ca
Raidan <92235772+Raidansz@users.noreply.github.com>
2024-09-09 13:36:06 +0200

    Initial Commit

---
+++ podcast-ios/podcast_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  podcast_iosApp.swift
+//  podcast-ios
+//
+//  Created by Raidan on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct podcast_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
