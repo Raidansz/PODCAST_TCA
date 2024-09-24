1566c54e1509c415bcd96a3417c563031f23c998
Raidan <92235772+Raidansz@users.noreply.github.com>
2024-09-24 17:57:03 +0200

    adding Tabbar

--- podcast-ios/PodcastContentView.swift
+++ podcast-ios/PodcastContentView.swift
@@ -12,10 +12,32 @@ import ComposableArchitecture
 struct PodcastContentView: App {
     var body: some Scene {
         WindowGroup {
-            HomeView(store: Store(initialState: HomeFeature.State()) {
+            TabBarView(store: Store(initialState: HomeFeature.State()) {
                 HomeFeature()
-            }
-            )
+            })
+        }
+    }
+}
+
+struct TabBarView: View {
+    let store: StoreOf<HomeFeature>
+
+    var body: some View {
+        TabView {
+            HomeView(store: store)
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+
+            HomeView(store: store)
+                .tabItem {
+                    Label("Explore", systemImage: "magnifyingglass")
+                }
+
+            HomeView(store: store)
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
         }
     }
 }
