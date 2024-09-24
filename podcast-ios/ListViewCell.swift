4346cdc9a1ce4f0602d75768470fe5e4696c1b55
Raidan <92235772+Raidansz@users.noreply.github.com>
2024-09-24 17:16:18 +0200

    fix podcast cell image

--- podcast-ios/ListViewCell.swift
+++ podcast-ios/ListViewCell.swift
@@ -16,20 +16,23 @@ struct ListViewCell: View {
                 if let image = phase.image {
                     image
                         .resizable()
-                        .frame(width: 108, height: 96)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 100, height: 100)
                         .cornerRadius(24)
                         .clipped()
                 } else {
+
                     Image(systemName: "waveform.badge.mic")
-                        .frame(width: 108, height: 96)
+                        .frame(width: 100, height: 100)
                         .cornerRadius(24)
                         .overlay(
                             RoundedRectangle(cornerRadius: 24)
                                 .stroke(Color.gray, lineWidth: 0.5)
                         )
-
                 }
             }
+            .frame(width: 100, height: 100)
+            .cornerRadius(24)
 
             VStack(alignment: .leading, spacing: 8) {
                 Text(podcast.title)
