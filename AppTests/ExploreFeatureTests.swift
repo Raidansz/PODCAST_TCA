2e7d1d4db9e0e27ddd305a0773768956f59001b4
Raidan <92235772+Raidansz@users.noreply.github.com>
2024-12-01 21:26:21 +0100

    explore screen tests

---
+++ AppTests/ExploreFeatureTests.swift
@@ -0,0 +1,16 @@
+//
+//  ExploreFeatureTests.swift
+//  AppTests
+//
+//  Created by Raidan on 2024. 12. 01..
+//
+
+import Testing
+
+struct ExploreFeatureTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
